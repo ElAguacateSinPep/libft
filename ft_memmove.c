8d1fb572b5bb427e7877940983b9fd526a131127
ElAguacateSinPep <guguimunoz@gmail.com>
2025-10-03 16:49:39 +0200

    funcion memmove posiblemente no definitiva

---
+++ ft_memmove.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amunoz-o <amunoz-o@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/03 15:37:28 by amunoz-o          #+#    #+#             */
+/*   Updated: 2025/10/03 16:47:20 by amunoz-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	*ft_memmove(void *dst, const void *src, size_t n)
+{
+	unsigned char			*d;
+	const unsigned char		*s;
+	size_t					i;
+
+	d = (unsigned char *)dst;
+	s = (const unsigned char *)src;
+	if ((dst == NULL) && (src == NULL))
+		return (NULL);
+	if (d < s)
+	{
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	else
+	{
+		while (n--)
+			d[n] = s[n];
+	}
+	return (dst);
+}
