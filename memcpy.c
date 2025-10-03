1af5283a7c5f57d1d53c264584cd362aab5dfe7b
ElAguacateSinPep <guguimunoz@gmail.com>
2025-10-03 15:27:58 +0200

    subida del archivo memcpy.c

--- memcpy.c
+++ memcpy.c
@@ -6,8 +6,27 @@
 /*   By: amunoz-o <amunoz-o@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/01 18:37:55 by amunoz-o          #+#    #+#             */
-/*   Updated: 2025/10/01 19:02:39 by amunoz-o         ###   ########.fr       */
+/*   Updated: 2025/10/03 15:27:18 by amunoz-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-hola
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*search;
+	unsigned char	*dest;
+	size_t			i;
+
+	if ((dst == NULL) && (src == NULL))
+		return (NULL);
+	search = (unsigned char *)src;
+	dest = (unsigned char *)dst;
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = search[i];
+		i++;
+	}
+	return (dest);
+}
