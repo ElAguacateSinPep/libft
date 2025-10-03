eab02a53a7d93f79a3fdcfe84728672067abf4ad
ElAguacateSinPep <guguimunoz@gmail.com>
2025-10-03 12:28:11 +0200

    prueba

---
+++ memset.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   memset.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amunoz-o <amunoz-o@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/01 18:12:44 by amunoz-o          #+#    #+#             */
+/*   Updated: 2025/10/01 18:29:36 by amunoz-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memset(void *b, int c, size_t len)
+{
+	int				i;
+	unsigned char	*bigc;
+
+	bigc = b;
+	i = 0;
+	while (len > 0)
+	{
+		bigc[i] = (char)c;
+		len--;
+		i++;
+	}
+	return (bigc);
+}
