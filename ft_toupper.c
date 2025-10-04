5280374e9a25dc884b8d44e77120734d6c974449
ElAguacateSinPep <guguimunoz@gmail.com>
2025-10-04 21:03:32 +0200

    subida de function toupper

--- ft_toupper.c
+++ ft_toupper.c
@@ -1,33 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: amunoz-o <amunoz-o@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/04 14:06:05 by amunoz-o          #+#    #+#             */
-/*   Updated: 2025/10/04 20:41:55 by amunoz-o         ###   ########.fr       */
+/*   Created: 2025/10/04 20:42:25 by amunoz-o          #+#    #+#             */
+/*   Updated: 2025/10/04 20:55:00 by amunoz-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	strlcat(char *dst, const char *src, size_t max)
+int	ft_toupper(int c)
 {
-	size_t	lendst;
-	size_t	j;
-
-	lendst = 0;
-	j = 0;
-	while (lendst < max && dst[lendst] != '\0')
-		lendst++;
-	if (lendst == max)
-		return (lendst + ft_strlen(src));
-	while ((lendst + j + 1) < max && src[j] != '\0')
-	{
-		dst[lendst + j] = src[j];
-		j++;
-	}
-	dst[lendst + j] = '\0';
-	return (lendst + ft_strlen(src));
+	if (c >= 'a' && c <= 'z')
+		c -= 32;
+	return (c);
 }
