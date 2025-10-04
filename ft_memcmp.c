0ff8dc84a697909fed199bd57f730dd52f4fbc1c
ElAguacateSinPep <guguimunoz@gmail.com>
2025-10-04 22:41:33 +0200

    subida archivo memcmp (revision)

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amunoz-o <amunoz-o@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/04 22:40:00 by amunoz-o          #+#    #+#             */
+/*   Updated: 2025/10/04 22:41:09 by amunoz-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*string1;
+	unsigned char	*string2;
+
+	string1 = (unsigned char *)s1;
+	string2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (string1[i] != string2[i])
+			return (string1[i] - string2[i]);
+		i++;
+	}
+	return (0);
+}
