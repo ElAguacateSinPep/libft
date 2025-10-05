b4a8fed1b568ea7d5ff34b4cbe1ff2447db2e559
ElAguacateSinPep <guguimunoz@gmail.com>
2025-10-05 16:42:11 +0200

    subida de la function strdup

---
+++ ft_strdup.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amunoz-o <amunoz-o@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/05 16:39:27 by amunoz-o          #+#    #+#             */
+/*   Updated: 2025/10/05 16:41:06 by amunoz-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	i;
+	size_t	len;
+	char	*cpy;
+
+	i = 0;
+	len = ft_strlen(s1);
+	cpy = (char *)malloc(ft_strlen((char *)s1) + 1);
+	if (cpy == NULL)
+		return (NULL);
+	while (i < len)
+	{
+		cpy[i] = s1[i];
+		i++;
+	}
+	cpy[i] = '\0';
+	return (cpy);
+}
+ 
