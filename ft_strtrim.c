24a6c5dceb4b77332b73120b25714bc7ae123f09
ElAguacateSinPep <guguimunoz@gmail.com>
2025-10-05 17:06:56 +0200

    subida de funcion strtrim (revision)

---
+++ ft_strtrim.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amunoz-o <amunoz-o@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/05 17:05:40 by amunoz-o          #+#    #+#             */
+/*   Updated: 2025/10/05 17:06:03 by amunoz-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	len;
+
+	if (!s1 || !set)
+		return (0);
+	while (*s1 && ft_strchr(set, *s1))
+		s1++;
+	len = ft_strlen(s1);
+	while (ft_strrchr(set, s1[len]) && len)
+		len--;
+	return (ft_substr(s1, 0, len + 1));
+}
