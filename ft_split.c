081fcde66fabba7571b542e159e7604e70a801b3
ElAguacateSinPep <guguimunoz@gmail.com>
2025-10-05 18:39:49 +0200

    subida de archivo split.c

---
+++ ft_split.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amunoz-o <amunoz-o@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/05 17:14:41 by amunoz-o          #+#    #+#             */
+/*   Updated: 2025/10/05 17:15:03 by amunoz-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static	void	ft_free(char **res)
+{
+	int	i;
+
+	i = -1;
+	while (res[++i])
+		free(res[i]);
+	free(res);
+}
+
+static	size_t	count_words(char const *s, char c)
+{
+	size_t	i;
+	size_t	words;
+
+	i = 0;
+	words = 0;
+	while (s[i])
+	{
+		if ((s[i + 1] == c || s[i + 1] == '\0') && s[i] != c)
+			words++;
+		i++;
+	}
+	return (words);
+}
+
+static	char	**write_result(char const *s, char c, char	**res)
+{
+	size_t	start;
+	size_t	i;
+	size_t	word;
+
+	start = 0;
+	i = 0;
+	word = 0;
+	while (s[i])
+	{
+		if ((s[i + 1] == c || s[i + 1] == '\0') && s[i] != c)
+		{
+			res[word] = ft_substr(s, start, i - start + 1);
+			if (!res[word])
+			{
+				ft_free(res);
+				return (0);
+			}
+			word++;
+		}
+		if (s[i] == c && (s[i + 1] != c || s[i + 1] != '\0'))
+			start = i + 1;
+		i++;
+	}
+	res[word] = 0;
+	return (res);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	size_t	i;
+	char	**res;
+
+	i = 0;
+	if (!s)
+		return (0);
+	res = malloc(sizeof(char *) * (count_words(s, c) + 1));
+	if (!res)
+		return (0);
+	res = write_result(s, c, res);
+	return (res);
+}
