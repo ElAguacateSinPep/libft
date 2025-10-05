95279911d9cb5eec61434bfcf58f9722072a717e
ElAguacateSinPep <guguimunoz@gmail.com>
2025-10-05 16:34:48 +0200

    arreglo de la norminette en la funcion calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,12 +6,11 @@
 /*   By: amunoz-o <amunoz-o@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 14:38:15 by amunoz-o          #+#    #+#             */
-/*   Updated: 2025/10/05 16:07:25 by amunoz-o         ###   ########.fr       */
+/*   Updated: 2025/10/05 16:34:02 by amunoz-o         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
 
 void	*ft_calloc(size_t count, size_t size)
 {
